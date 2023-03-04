a297d3ae1d1301ad06cc6c29216af92cf35cfc35
Cleazyy <cleazy2002@gmail.com>
2023-03-04 17:28:12 +0100

    remove testing program

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,42 +6,13 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 18:15:37 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/04 17:26:48 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/04 17:27:56 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-pthread_mutex_t lock;
-int count = 0;
-
-void 	*routine()
-{
-	int i;
-
-	i = 0;
-	while (i < 1000000)
-	{
-		pthread_mutex_lock(&lock);
-		count++;
-		i++;
-		pthread_mutex_unlock(&lock);
-	}
-	return (NULL);
-}
-
 int	main(void)
 {
-	pthread_t	thread1;
-	pthread_t	thread2;
-
-	pthread_mutex_init(&lock, NULL);
-	pthread_create(&thread1, NULL, &routine, NULL);
-	pthread_create(&thread2, NULL, &routine, NULL);
-
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-	pthread_mutex_destroy(&lock);
-
-	printf("count: %d\n", count);
+	return (0);
 }
