edaf6298d0464a82dfdcc92099b55c8872f939b1
Cleazyy <cleazy2002@gmail.com>
2023-03-04 17:27:01 +0100

    create struct

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 18:17:30 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/04 17:10:55 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/04 17:25:51 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 # include <stdio.h>
 # include <pthread.h>
+
+typedef struct s_philo
+{
+	struct s_data	*data;
+	int				id;
+}	t_philo;
+
+typedef struct s_data
+{
+	int		nb_philos;
+	int		t_time;
+	int		t_eat;
+	int		t_sleep;
+	int		nb_meals
+}	t_data;
 
 #endif
