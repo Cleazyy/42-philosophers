4d975554537698a7bcbc973ac3f0006d235c4d44
Cleazyy <cleazy2002@gmail.com>
2023-03-06 08:39:49 +0100

    update

--- example.c
+++ example.c
@@ -1,30 +1,45 @@
-int cont = 0;
-pthread_mutex_t lock;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   example.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 08:36:49 by fluchten          #+#    #+#             */
+/*   Updated: 2023/03/06 08:39:36 by fluchten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void  *routine()
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t	g_lock;
+int				g_count = 0;
+
+void	*routine(void *arg)
 {
-  int i;
+	int	i;
 
-  i = -1;
-  while (++i < 1000000)
-  {
-    pthread_mutex_lock(&lock);
-  	  cont++;
-    pthread_mutex_unlock(&lock);
-  }
-  return (NULL);
+	i = -1;
+	while (++i < 1000000)
+	{
+		pthread_mutex_lock(&g_lock);
+		g_count++;
+		pthread_mutex_unlock(&g_lock);
+	}
+	return (NULL);
 }
 
-int main()
+int	main(void)
 {
-  pthread_t tid1, tid2;
+	pthread_t	tid1;
+	pthread_t	tid2;
 
-  pthread_mutex_init(&lock, NULL);
-  pthread_create(&tid1, NULL, &routine, NULL);
-  pthread_create(&tid2, NULL, &routine, NULL);
-
-  pthread_join(tid1, NULL);
-  pthread_join(tid2, NULL);
-  pthread_mutex_destroy(&lock);
-  printf("cont: %d\n", cont);
+	pthread_mutex_init(&g_lock, NULL);
+	pthread_create(&tid1, NULL, &routine, NULL);
+	pthread_create(&tid2, NULL, &routine, NULL);
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+	pthread_mutex_destroy(&g_lock);
+	printf("g_count: %d\n", g_count);
 }
