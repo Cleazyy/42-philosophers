83da49ab0cac9320fb61d132d5b86bbb0aeef9b8
Cleazyy <cleazy2002@gmail.com>
2023-03-06 22:19:01 +0100

    remove comment

--- philo/srcs/initialization/initialization.c
+++ philo/srcs/initialization/initialization.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 18:57:57 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/06 22:02:06 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/06 22:17:54 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int	initialization(t_data *data)
 	if (!init_threads(data))
 		return (0);
 	death_monitoring(data);
-	// free_everythings(data);
+	free_everythings(data);
 	return (1);
 }
